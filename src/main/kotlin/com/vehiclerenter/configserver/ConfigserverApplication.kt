f13c42aa6928125eb951c0f67c0db682a4981b6a
oenegm <oenegm@gmail.com>
2023-08-27 23:08:31 +0300

    init

---
+++ src/main/kotlin/com/vehiclerenter/configserver/ConfigserverApplication.kt
@@ -0,0 +1,11 @@
+package com.vehiclerenter.configserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConfigserverApplication
+
+fun main(args: Array<String>) {
+    runApplication<ConfigserverApplication>(*args)
+}
